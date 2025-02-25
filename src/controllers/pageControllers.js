6b101071dfdf542dc3e953ee8e875b6be8271457
Jarne <jarne.demeyer05@hotmail.com>
2025-02-25 12:57:16 +0100

    adding templates to pages folder

--- src/controllers/pageControllers.js
+++ src/controllers/pageControllers.js
@@ -1,26 +1,26 @@
 export const home = (req, res) => {
-  res.render("home", {
+  res.render("pages/home", {
     title: "Dinos around the world",
     content: "Welcome to the world of dinos",
   });
 };
 
 export const about = (req, res) => {
-  res.render("default", {
+  res.render("pages/default", {
     title: "about us",
     content: "We love dinos",
   });
 };
 
 export const contact = (req, res) => {
-  res.render("contact", {
+  res.render("pages/contact", {
     title: "Contact",
     content: "Contact us at 234 2344 223",
   });
 };
 
 export const privacy = (req, res) => {
-  res.render("default", {
+  res.render("pages/default", {
     title: "Privacy Policy",
     content: "Dinos are protective of their privacy",
   });
